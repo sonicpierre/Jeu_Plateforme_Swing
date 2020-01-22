dca06350e163be93c48547e274d87ea09c4975ea
Jérémy Zangla <zangla.jeremy@gmail.com>
2020-01-22 23:13:09 +0100

    Z Event

--- src/jereme/control/KeyController.java
+++ src/jereme/control/KeyController.java
@@ -1,5 +1,28 @@
 package jereme.control;
 
-public class KeyController {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+public class KeyController extends KeyAdapter{
+
+	boolean isZPressed = false;
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		super.keyPressed(e);
+		if (e.getKeyCode() == KeyEvent.VK_Z)
+			isZPressed = true;
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		super.keyReleased(e);
+		if (e.getKeyCode() == KeyEvent.VK_Z)
+			isZPressed = false;
+	}
+	
+	
+	public boolean getZState() {
+		return isZPressed;
+	}
 }
